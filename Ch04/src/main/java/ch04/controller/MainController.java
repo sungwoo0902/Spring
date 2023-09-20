e8e0d585f579f5016ba8a061275444e8c3c25712
sungwoo0902 <rhtjddn2@gmail.com>
2023-09-20 12:51:22 +0900

    MVC 실습하기

---
+++ Ch04/src/main/java/ch04/controller/MainController.java
@@ -0,0 +1,41 @@
+package ch04.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = {"/", "/index"}, method=RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String hello() {
+		return "hello";
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+	
+	@GetMapping("/greeting")
+	public String greeting() {
+		return "greeting";
+	}
+	
+	@GetMapping("/redirect")
+	public String redirect() {
+		return "redirect:/annotation/param";
+	}
+	
+	@GetMapping("/forward")
+	public String forward() {
+		return "forward:/annotation/model";
+	}
+}
+
+
