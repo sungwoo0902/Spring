3fe8f7c5f066e4e3263ef17533a7207db5d69d7b
sungwoo0902 <rhtjddn2@gmail.com>
2023-10-05 12:49:05 +0900

    시큐리티 실습

---
+++ Ch09/src/main/java/kr/ch09/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package kr.ch09.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kr.ch09.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String>{
+	
+	// 아이디 비밀번호를 조회하는 쿼리 메서드
+	public UserEntity findUserEntityByUidAndPass(String uid, String pass);
+
+}
